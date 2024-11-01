15d63af950b2fe52fe96b75e58797f36d6bf63aa
karenbela <karenkudemus94889@gmail.com>
2024-11-01 09:09:21 +0800

    menambahkan file app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
